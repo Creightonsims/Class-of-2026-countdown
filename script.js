4fce6839b74bf2de8ac9ea1230fa7abbb0bf6fa3
Creightonsims <creightonsims@yahoo.com>
2025-07-10 18:33:06 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -20,14 +20,12 @@ function updateCountdown() {
   const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  const milliseconds = Math.floor(distance % 1000);
 
   document.getElementById("days").textContent = days.toString().padStart(2, "0");
   document.getElementById("hours").textContent = hours.toString().padStart(2, "0");
   document.getElementById("minutes").textContent = minutes.toString().padStart(2, "0");
   document.getElementById("seconds").textContent = seconds.toString().padStart(2, "0");
-  document.getElementById("milliseconds").textContent = milliseconds.toString().padStart(3, "0");
 }
 
-setInterval(updateCountdown, 50);
+setInterval(updateCountdown, 1000);
 updateCountdown();
